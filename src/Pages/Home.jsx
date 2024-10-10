36bc0bc80abeb6be60abdd6a72536eea59e4e6fa
akshayvsanthosh <akshaysanthosh12345@gmail.com>
2024-10-10 12:34:25 +0530

    basic setup completed

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Home() {
+    return (
+        // container div
+        <div className='homeContainer'>
+            {/* blur div */}
+            <div className='homeContainerBlur'>
+                Hello
+            </div>
+        </div>
+    )
+}
+
+export default Home
